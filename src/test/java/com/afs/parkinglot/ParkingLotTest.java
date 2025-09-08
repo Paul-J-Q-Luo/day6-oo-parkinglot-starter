97b2377c05826613a38a13e1289d039dd167df23
Paul J Q Luo <paul.j.q.luo@oocl.com>
2025-09-08 16:38:32 +0800

    test: enable unit test for fetching cars with invalid tickets in ParkingLot

--- src/test/java/com/afs/parkinglot/ParkingLotTest.java
+++ src/test/java/com/afs/parkinglot/ParkingLotTest.java
@@ -43,19 +43,19 @@ public class ParkingLotTest {
         Assertions.assertEquals(car1, fetchedCar1);
         Assertions.assertEquals(car2, fetchedCar2);
     }
-//
-//    @Test
-//    public void should_return_null_when_fetch_given_parking_lot_with_parked_car_and_wrong_ticket() {
-//        ParkingLot parkingLot = new ParkingLot();
-//        Car car1 = new Car("123456");
-//        Car car2 = new Car("234567");
-//        Ticket ticket = new Ticket(2, car2, parkingLot);
-//        parkingLot.park(car1);
-//
-//        Car fetchedCar = parkingLot.fetch(ticket);
-//
-//        Assertions.assertNull(fetchedCar);
-//    }
+
+    @Test
+    public void should_return_null_when_fetch_given_parking_lot_with_parked_car_and_wrong_ticket() {
+        ParkingLot parkingLot = new ParkingLot();
+        Car car1 = new Car("123456");
+        Car car2 = new Car("234567");
+        Ticket ticket = new Ticket(2, car2, parkingLot);
+        parkingLot.park(car1);
+
+        Car fetchedCar = parkingLot.fetch(ticket);
+
+        Assertions.assertNull(fetchedCar);
+    }
 //
 //    @Test
 //    public void should_return_null_when_fetch_given_parking_lot_with_parked_car_and_ticket_is_used() {
